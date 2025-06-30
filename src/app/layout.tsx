a8d6496fce976df760fa8c7087a87d937e36bd2c
Ieeeza <cesardermer1@gmail.com>
2025-06-30 10:45:27 -0300

    portfolio basico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "César Pereira - Portfolio",
+  description: "É, esse sou eu!",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
